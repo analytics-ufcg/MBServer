c1496059fc5460dbefd7f1157ba772d89acfc1ee
MatheusMaciel <teu.araujo@gmail.com>
2017-05-30 09:06:41 -0300

    Making scripts beauty.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express'),
   port = process.env.PORT || 30000,
   bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 var routes = require('./api/routes/mb-router');
