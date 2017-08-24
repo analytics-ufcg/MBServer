d6ab57050487986aede6428a7ad5e785e7819226
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-08-24 11:12:58 -0300

    Starting integration of OTP and BTR 2.0

--- python/src/mbserver.py
+++ python/src/mbserver.py
@@ -2,13 +2,14 @@
 import tornado.ioloop
 import tornado.web
 import tornado.escape
+from tornado import gen
+from tornado.httpclient import AsyncHTTPClient
+from tornado.options import define, options, parse_command_line
 
 from spark_handler import SparkHandler
 
-from tornado.options import define, options, parse_command_line
-
-import os
-import sys
+import btr_otp_config
+import urllib
 
 define("port", default=8888, help="run on the given port", type=int)
 
@@ -25,14 +26,36 @@ class IndexHandler(tornado.web.RequestHandler):
         self.finish()
 
 
+class JoinBTRandOTPHandler(tornado.web.RequestHandler):
+
+    @gen.coroutine
+    def post(self):
+        # query example: {
+        #   "fromPlace": "-25.39211,-49.22613",
+        #   "toPlace": "-25.45102,-49.28381",
+        #   "mode": "TRANSIT,WALK",
+        #   "date": "04/03/2017",
+        #   "time": "17:20:00"
+        #   }
+        request_params = tornado.escape.json_decode(self.request.body)
+
+        query = btr_otp_config.OTP_LINK + urllib.urlencode(request_params)
+
+        http_client = AsyncHTTPClient()
+        response = yield http_client.fetch(query)
+
+        self.write(response.body)
+
+
 app = tornado.web.Application([
     (r'/previsao', IndexHandler),
+    (r'/btr_routes_plans', JoinBTRandOTPHandler)
 ])
 
 
 def startUp():
     global sparkHandler
-    modelPath = "hdfs://localhost:9000/btr/ctba/models/trip_duration"
+    modelPath = btr_otp_config.MODEL_PATH
     appName = "Duration Prediction"
     sparkHandler = SparkHandler(appName, modelPath)
     parse_command_line()
