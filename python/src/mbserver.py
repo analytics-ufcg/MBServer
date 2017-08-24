8932940582dd958a4a119bd1f244b5f751aea3fa
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-08-24 17:59:28 -0300

    Continuing OTP and BTR integration

--- python/src/mbserver.py
+++ python/src/mbserver.py
@@ -7,6 +7,7 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.options import define, options, parse_command_line
 
 from spark_handler import SparkHandler
+from datetime import datetime, date
 
 import btr_otp_config
 import urllib
@@ -28,8 +29,67 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class JoinBTRandOTPHandler(tornado.web.RequestHandler):
 
+    def set_variables(self, request_params):
+        self.date_fmt = datetime.strptime(request_params["date"], "%m/%d/%Y")
+        self.time = request_params["time"]
+        self.hour = self.time.split(":")[0]
+        self.period_origin = "morning" if self.hour < 12 else "afternoon" if self.hour < 18 else "night"
+        self.is_rush_hour = 1 if self.hour in [6, 7, 11, 12, 17, 18] else 0
+
+        self.weekday = self.date_fmt.weekday()
+        wd = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+        self.weekdays_str = wd[self.weekday]
+
+        self.week_of_year = self.date_fmt.isocalendar()[1]
+        self.is_holiday = 1 if self.date_fmt.month in [1, 6, 7, 12] else 0
+        self.is_weekend = 1 if self.weekday in [5, 6] else 0
+        self.is_regular_day = 1 if self.weekday in [1, 2, 3] else 0
+
+    def feature_extractor(self, leg):
+        features = dict()
+
+        features["distance"] = leg["distance"]
+
+        features["hourOrig"] = int(self.hour)
+        features["periodOrig"] = self.period_origin
+        features["isRushOrig"] = self.is_rush_hour
+
+        features["weekDay"] = self.weekday
+        features["weekOfYear"] = self.week_of_year
+        features["dayOfMonth"] = self.date_fmt.day
+        features["month"] = self.date_fmt.month
+        features["isHoliday"] = self.is_holiday
+        features["isWeekend"] = self.is_weekend
+        features["isRegularDay"] = self.is_regular_day
+
+        features["route"] = leg["route"]
+
+        features["shapeLatOrig"] = leg["from"]["lat"]
+        features["shapeLonOrig"] = leg["from"]["lon"]
+        features["shapeLatDest"] = leg["to"]["lat"]
+        features["shapeLonDest"] = leg["to"]["lon"]
+
+        features["busStopIdOrig"] = int(leg["from"]["stopId"].split(":")[1])
+        features["busStopIdDest"] = int(leg["to"]["stopId"].split(":")[1])
+
+        return features
+
+    def get_btr_duration(self, leg):
+        if leg["mode"] == "WALK":
+            return leg["duration"]
+        else:
+            a = sparkHandler.predict(self.feature_extractor(leg))
+            print a
+            return a
+
     def get_btr_prediction(self, otp_data):
-        pass
+        for it in otp_data["plan"]["itineraries"]:
+            durations = map(self.get_btr_duration, it["legs"])
+
+            #it["btr-duration"] = reduce(lambda l1, l2: l1 + l2, durations)
+
+        return otp_data
+
 
     @gen.coroutine
     def post(self):
@@ -48,6 +108,10 @@ class JoinBTRandOTPHandler(tornado.web.RequestHandler):
         response = yield http_client.fetch(query)
 
         otp_data = tornado.escape.json_decode(response.body)
+
+        self.set_variables(request_params)
+
+        self.get_btr_prediction(otp_data)
 
         self.write(response.body)
 
