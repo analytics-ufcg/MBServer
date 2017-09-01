b9a7ca6ed8ba6faa49298069ea399dfa17b7a93f
odwl66 <orion.lima@ccc.ufcg.edu.br>
2017-09-01 11:31:49 -0300

    Ordering shapes by size

--- python/src/handlers/spark_handler.py
+++ python/src/handlers/spark_handler.py
@@ -41,7 +41,11 @@ class SparkHandler:
 
             routes_stops[route][shape_id].append((bus_stop, distance))
 
-        return routes_stops
+        routes_stops_final = dict()
+        for route in routes_stops:
+            routes_stops_final[route] = sorted(routes_stops[route].values(), key=len, reverse=True)
+
+        return routes_stops_final
 
     def predict(self, test_data):
         # predicting_json_example = {"periodOrig": "morning", "weekDay": "Mon", "route": "203",
@@ -98,8 +102,7 @@ class SparkHandler:
         for row in df_dict:
             route, bus_stop_orig, bus_stop_dest = row["route"], row["busStopIdOrig"], row["busStopIdDest"]
             found_shape = False
-            for shape in self.routes_stops[route]:
-                shape_stops_list = self.routes_stops[route][shape]
+            for shape_stops_list in self.routes_stops[route]:
                 if self.both_on_list(bus_stop_orig, bus_stop_dest, shape_stops_list):
                     found_shape = True
                     i = 0
@@ -124,6 +127,8 @@ class SparkHandler:
 
             if not found_shape:
                 new_df_list.append(row)
+
+        print new_df_list
 
         new_df = self.sc.parallelize(new_df_list).toDF()
 
